10df4817e4b56d4d9824cb9d13dc825d36d42a74
zahid hasan <shuvo.cse6.bu@gmail.com>
2023-11-05 22:24:26 +0600

    cart total calculation and set localstorage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,20 +4,30 @@ import "./Cart.css";
 const Cart = ({ cart }) => {
   // console.log(cart);
   let total = 0;
+  let totalShipping = 0;
+  let quantity =0;
+
   for (const product of cart) {
-    total = total + product.price;
+    if(product.quantity===0){
+      product.quantity=1
+    }
+    total = total + product.price*product.quantity;
+    totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
  
     // console.log(product);
   }
+  const tax=(total*7)/100;
+  const granTotal=total+totalShipping+tax;
  
   return (
     <div className="cart-summary">
       <h2>Order Summary</h2>
-      <p>Selected Items :{cart.length}</p>
-      <p>Total Price:{total} </p>
-      <p>Total Shipping: {}</p>
-      <p>Tax: {}</p>
-      <h5>Grand Total :{}</h5>
+      <p>Selected Items :{quantity}</p>
+      <p>Total Price: ${total} </p>
+      <p>Total Shipping: ${totalShipping}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h5>Grand Total : ${granTotal.toFixed(2)}</h5>
     </div>
   );
 };
