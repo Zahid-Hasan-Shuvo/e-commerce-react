a5397dd349c3e00bc3253160ec12c587d46d1067
zahid hasan <shuvo.cse6.bu@gmail.com>
2023-11-12 07:45:59 +0600

    card delete from order list

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../../assets/utilities/fakedb';
 
 const Orders = () => {
     const cart= useLoaderData()
     // console.log(products);
+    const [cart1, setCart1]=useState(cart)
+
+    const handleRemoveFromCart=(id)=>{
+const remaining= cart1.filter(product=>product.id !== id)
+setCart1(remaining)
+removeFromDb(id)
+    }
     return (
         <div className='shop-container'>
 
      <div className='review-container'>
 {
-    cart.map(product=> <ReviewItem key={product.id} product={product}></ReviewItem>)
+    cart1.map(product=> <ReviewItem key={product.id} product={product} handleRemoveFromCart={handleRemoveFromCart}></ReviewItem>)
 }
      </div>
 
      <div className='cart-container'>
-<Cart cart={cart}></Cart>
+<Cart cart={cart1}></Cart>
      </div>
 
         </div>
